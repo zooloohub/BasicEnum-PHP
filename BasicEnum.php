14179ce44c69a4456ddbc2a698feeed1b2a1d7c8
ductiletoaster <briangeb@gmail.com>
2015-04-15 08:44:48 -0700

    Adding Basic enum class

---
+++ BasicEnum.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * PixelOven Framework
+ *
+ * @author Brian Gebel <brian@pixeloven.com>
+ * @link http://www.pixeloven.com/
+ * @copyright 2015 PixelOven
+ *
+ * Allows for basic enumeration in PHP
+ * 
+ */
+
+
+/**
+ * Mimics Enumeration 
+ */
+abstract class BasicEnum {
+
+	// Static Variables
+	private static $constCacheArray = NULL;
+
+	/**
+	 * Checks if name exists in enum
+	 * @param: (string)
+	 * @param: (bool)
+	 * @return: (bool)
+	 */
+	public static function isValidName($name, $strict = false) {
+		$constants = self::getConstants();
+		if ($strict)
+			return array_key_exists($name, $constants);
+		$keys = array_map('strtolower', array_keys($constants));
+		return in_array(strtolower($name), $keys);
+	}
+
+	/**
+	 * Checks if value exists in enum
+	 * @param: (mixed)
+	 * @return: (bool)
+	 */
+	public static function isValidValue($value) {
+		$values = array_values(self::getConstants());
+		return in_array($value, $values, $strict = true);
+	}
+
+	/**
+	 * Returns value with a given name
+	 * @param: (string)
+	 * @return: (mixed)
+	 */
+	public static function getValueByName($name) {
+		$constants = self::getConstants();
+		if(array_key_exists($name, $constants))
+			return $constants[$name];
+		return false;
+	}
+
+	/**
+	 * Returns the name of the constant with a given value
+	 * If Enum has non-unqiue values then the first instance is returned
+	 * Should have it return array of constants with given value
+	 * @param: (string)
+	 * @return: (mixed)
+	 */
+	public static function getNameByValue($value) {
+		$constants = self::getConstants();
+		$flip = array_flip($constants);
+		if (array_key_exists($value, $flip))
+			return $flip[$value];
+		return false;
+	}
+
+	/**
+	 * Gets constant from defined enum
+	 * @return: (mixed)
+	 */
+	private static function getConstants() {
+		if (self::$constCacheArray == NULL) {
+			self::$constCacheArray = [];
+		}
+		$calledClass = get_called_class();
+		if (!array_key_exists($calledClass, self::$constCacheArray)) {
+			$reflect = new ReflectionClass($calledClass);
+			self::$constCacheArray[$calledClass] = $reflect->getConstants();
+		}
+		return self::$constCacheArray[$calledClass];
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # BasicEnum-PHP
 Mimics enumeration with inheritable class
+
+```php
+// Also consider making final instead of abstract
+abstract class DaysOfWeek extends BasicEnum {
+    const Sunday = 0;
+    const Monday = 1;
+    const Tuesday = 2;
+    const Wednesday = 3;
+    const Thursday = 4;
+    const Friday = 5;
+    const Saturday = 6;
+}
+
+DaysOfWeek::isValidName('Humpday');                  // false
+DaysOfWeek::isValidName('Monday');                   // true
+DaysOfWeek::isValidName('monday');                   // true
+DaysOfWeek::isValidName('monday', $strict = true);   // false
+DaysOfWeek::isValidName(0);                          // false
+
+DaysOfWeek::isValidValue(0);                         // true
+DaysOfWeek::isValidValue(5);                         // true
+DaysOfWeek::isValidValue(7);                         // false
+DaysOfWeek::isValidValue('Friday');                  // false
+```
+## Credit
+Much of the code and examples should be credited to Brian Cline and others. Orignal thread can be seen at the link below
+http://stackoverflow.com/questions/254514/php-and-enumerations
